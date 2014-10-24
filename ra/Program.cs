69d609074810458a5407ccdef79ca8b3cbcdf887
Shawn Doucet <shawndoucet@gmail.com>
2014-10-24 11:47:40 -0500

    messages

---
+++ ra/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RunAsWrapper.Core.Helpers;
+
+namespace RunAsWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            var parser = new CommandLineParser();
+            parser.ParseAndExecute(args);          
+
+        }
+    }
+}
