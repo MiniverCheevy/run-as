9b123e4aae9646e433b6099b40a2c8d9926856a3
Shawn Doucet <shawndoucet@gmail.com>
2014-10-24 11:18:48 -0500

    Revert "missing files"
    This reverts commit feb50587e01719db60e2916e492345f0c05da63a.

--- Hosting/postBuild.bat
+++
@@ -1,2 +0,0 @@
-rmdir /S /Q "C:\DropBox\Lib\Projects\ra - Copy\build-outputs\web\"
-mklink /D "C:\DropBox\Lib\Projects\ra - Copy\build-outputs\web\" "C:\DropBox\Lib\Projects\ra - Copy\Hosting\web\" 
