dfd32d885649b162bd1e4d8e75f201d2bd7a467f
Shawn Doucet <shawndoucet@gmail.com>
2014-10-30 16:17:17 -0500

    rename reorganize

--- Hosting/postBuild.bat
+++
@@ -1,2 +0,0 @@
-rmdir /S /Q "C:\DropBox\Lib\Projects\ra\build-outputs\web\"
-mklink /D "C:\DropBox\Lib\Projects\ra\build-outputs\web\" "C:\DropBox\Lib\Projects\ra\Hosting\web\" 
--- Hosting/web/app/voodoo/voodoo.js
+++
@@ -1,6 +0,0 @@
-﻿(function() {
-    angular.module('voodoo.ui', ['ui.bootstrap', 'voodoo.ui.pager', 'voodoo.ui.sorter',
-        'voodoo.ui.datePicker', 'serverError', 'ngReallyClickModule', 'voodoo.ui.database',
-        'voodoo.ui.utility'
-    ]);
-}())
